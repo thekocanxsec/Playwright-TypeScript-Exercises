d0f3d87c96b865d19f8d69c8d5dbfbe29ccbe3ca
thekocanxsec <imran.kocan@gmail.com>
2026-08-11 15:25:16 +0200

    codegen positive-negative tests

--- codegen/codegen-introduction.spec.ts
+++ codegen/codegen-introduction.spec.ts
@@ -1,15 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Positive login test', async ({ page }) => {
   await page.goto('http://the-internet.herokuapp.com/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!!');
-  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
   await page.getByRole('button', { name: ' Login' }).click();
-  await expect(page.locator('#flash')).toContainText('Your password is invalid! ×');
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+  await page.getByRole('link', { name: 'Logout' }).click();
+});
+
+
+test('Negative login test', async ({ page }) => {
+  await page.goto('http://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('bad_username');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('bad_password');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.locator('#flash')).toContainText('Your username is invalid!');
 });
