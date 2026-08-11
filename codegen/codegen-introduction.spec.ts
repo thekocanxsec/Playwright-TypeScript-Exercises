284e04be745ff34f69185b1fed83d9eaf5b32a5c
thekocanxsec <imran.kocan@gmail.com>
2026-08-11 15:30:04 +0200

    test.describe

--- codegen/codegen-introduction.spec.ts
+++ codegen/codegen-introduction.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('Positive login test', async ({ page }) => {
+test.describe("Login Tests", () => {
+
+  test('Positive login test', async ({ page }) => {
   await page.goto('http://the-internet.herokuapp.com/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
@@ -20,4 +22,5 @@ test('Negative login test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('bad_password');
   await page.getByRole('button', { name: ' Login' }).click();
   await expect(page.locator('#flash')).toContainText('Your username is invalid!');
+});
 });
